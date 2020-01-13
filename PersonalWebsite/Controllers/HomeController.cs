7a16e520bddd0b02e2a26840d84902803d74fa46
ilona <ilonabarashkina@gmail.com>
2020-01-12 19:46:43 -0500

    First commit

---
+++ PersonalWebsite/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using PersonalWebsite.Models;
+
+namespace PersonalWebsite.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //By Adding a new ation I can load a new view
+        public IActionResult About()
+        {
+            //return View here, if view isn't specified it defaults to the Action Name
+            return View();
+        }
+
+        //add portfolio action
+        public IActionResult Portfolio()
+        {
+            //return iew here, if view isn't specified it defaults to the Action Name
+            return View();
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
