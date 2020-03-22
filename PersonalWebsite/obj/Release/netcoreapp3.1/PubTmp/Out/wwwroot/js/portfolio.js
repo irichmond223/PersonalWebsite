535ff55d7b39d2cce34a200923d22db021aa8ddd
ilona <ilonabarashkina@gmail.com>
2020-03-22 18:20:59 -0400

    Updated css, html

---
+++ PersonalWebsite/obj/Release/netcoreapp3.1/PubTmp/Out/wwwroot/js/portfolio.js
@@ -0,0 +1,15 @@
+﻿
+
+function addLink() {
+
+    // Create anchor element. 
+    var a = document.createElement('a');
+
+
+    // Set the href property. 
+    a.href = "https://github.com/irichmond223/MidtermPOS";
+
+    // Append the anchor element to the body. 
+    document.body.appendChild(a);  
+
+}
